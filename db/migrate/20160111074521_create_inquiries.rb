33ec87a9bb920be81ddc97dd1685a460189afebc
Kazuhiro Ikeda <ikebright@gmail.com>
2016-01-11 07:54:12 +0000

    マイグレーション実行

--- db/migrate/20160111074521_create_inquiries.rb
+++ db/migrate/20160111074521_create_inquiries.rb
@@ -1,8 +1,8 @@
 class CreateInquiries < ActiveRecord::Migration
   def change
     create_table :inquiries do |t|
-      t.strings :name
-      t.strings :email
+      t.string :name
+      t.string :email
       t.text :message
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104041200) do
+ActiveRecord::Schema.define(version: 20160111074521) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inquiries", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
