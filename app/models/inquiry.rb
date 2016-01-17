a043cb3a6d9028aac686c540c5ef34c583c40006
Kazuhiro Ikeda <ikebright@gmail.com>
2016-01-17 07:32:14 +0000

    validation add

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,7 +1,17 @@
 class Inquiry < ActiveRecord::Base
 
+  #名前の制限
   validates :name,presence: true
+
+  #Eメールの制限
   validates :email,presence: true
+  validates :email, uniqueness: true
+  VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+
+  #メッセージの制限
   validates :message,presence: true
+
+
 
 end
