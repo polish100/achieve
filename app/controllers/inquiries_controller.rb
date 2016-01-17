2cc6eecdd93d6816fa0f8653281f4072664dc09e
Kazuhiro Ikeda <ikebright@gmail.com>
2016-01-17 06:18:38 +0000

    元に戻した

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,6 +1,5 @@
 class InquiriesController < ApplicationController
   def index
-    # 入力画面を表示
     @inquiry = Inquiry.new
     #render :action => 'index'
   end
@@ -23,6 +22,7 @@ class InquiriesController < ApplicationController
   def thanks
     Inquiry.create(inquiry_params)
   end
+
 
   private
 
