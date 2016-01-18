cb09b69f1a79f712138616563557e9e4dea1eca5
Kazuhiro Ikeda <ikebright@gmail.com>
2016-01-18 16:14:07 +0000

    ログイン画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'inquiries' => 'inquiries#index'
   post 'inquiries' => 'inquiries#index'
 #  post 'inquiries/create' => 'inquiries#create'
   post 'inquiries/confirm' => 'inquiries#confirm'
   post 'inquiries/thanks' => 'inquiries#thanks'
-  root 'top#index'
+#  root 'top#index'
+  root to: "top#index"
   resources :blogs
 end
 
