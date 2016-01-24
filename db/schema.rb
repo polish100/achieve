ed8ee58aa3fb9425dbec3ce140cfa26202073fa8
Kazuhiro Ikeda <ikebright@gmail.com>
2016-01-24 07:05:29 +0000

    day5kadai

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118073346) do
+ActiveRecord::Schema.define(version: 20160123155917) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160118073346) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
