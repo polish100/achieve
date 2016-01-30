61ebf54de73d09d41fab12c8981bfc329f423549
Kazuhiro Ikeda <ikebright@gmail.com>
2016-01-30 10:01:59 +0000

    ユーザページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
   devise_for :users
+  resources :users, only: [:index, :show]
   get 'inquiries' => 'inquiries#index'
   post 'inquiries' => 'inquiries#index'
 #  post 'inquiries/create' => 'inquiries#create'
