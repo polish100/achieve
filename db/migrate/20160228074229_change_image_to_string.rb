ec43e206556a0485aebd6b720e135cf067e44b6a
Kazuhiro Ikeda <ikebright@gmail.com>
2016-02-28 10:23:44 +0000

    migration file delete

--- db/migrate/20160228074229_change_image_to_string.rb
+++
@@ -1,9 +0,0 @@
-class ChangeImageToString < ActiveRecord::Migration
-def up
-    change_column :users, :image, :string
-  end
-
-  def down
-    change_column :users, :image, :binary
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228074229) do
+ActiveRecord::Schema.define(version: 20160227121956) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
