04125270379bb2f76a07d92e95e5ca9680f63911
polish100 <ikebright@gmail.com>
2016-05-23 21:03:50 +0900

    ルーティング追加したが不要なので元に戻した

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     passwords: "users/passwords",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
-  #resources :users, only: [:index, :show]
+#  resources :users, only: [:index, :show, :edit, :update]
   resources :users do
         member { get :image }
   end
