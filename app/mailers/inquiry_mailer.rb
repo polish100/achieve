f2bcc0e217e764d2b59eeaca2e91afb916642bba
polish100 <ikebright@gmail.com>
2016-06-04 12:16:01 +0900

    day10_6

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -6,7 +6,7 @@ class InquiryMailer < ActionMailer::Base
   def received_email(inquiry)
     @inquiry = inquiry
 #    binding.pry
-   mail to: @inquiry.email, subject: '【Achieve】'+@inquiry.subject
+   mail to: @inquiry.email, subject: 'お問い合わせを承りました'
   end
 
 end
