bce9cbcf803d906ebce089d057aed20c32139cb1
ikeda <ikebright@gmail.com>
2016-06-19 15:14:43 +0900

    aws env del

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,10 +14,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS',
-    #  :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],
-    #  :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],
-      :aws_access_key_id      => 'AKIAJA3STXAOYNKQYANA',
-      :aws_secret_access_key  => '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',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
       :region                 => 'ap-northeast-1',
       :path_style             => true
     }
