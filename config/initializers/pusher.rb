e0b96e026e189d93bbf55991ef9eabd88bb32788
ikeda <ikebright@gmail.com>
2016-08-14 17:50:30 +0900

    DIVE19_4

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -5,6 +5,3 @@ Pusher.key = ENV['PUSHER_ACCESS_KEY']
 Pusher.secret = ENV['PUSHER_ACCESS_KEY_SECERT']
 Pusher.logger = ENV['PUSHER_APP_ID']
 Pusher.encrypted = true
-
-Pusher.url = "https://4369b80e9ca90ea62e58:97ac02fd43484f8d1133@api.pusherapp.com/apps/144559"
-Pusher.logger = Rails.logger
